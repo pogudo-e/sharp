5ca647d87aa8dec82ba90ef73e99fd16339c81b7
Evgeny Pogudo <sensesbit@gmail.com>
2022-07-28 21:22:59 +0300

    example 005

--- example004/Program.cs
+++ example004/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 10;
-double numberB = 7;
-Console.WriteLine(numberA / numberB);
+﻿double numberA = new Random().Next(1,10);
+Console.Write(numberA + " + ");
+double numberB = new Random().Next(1,10);
+Console.Write(numberB + " = ");
+Console.WriteLine(numberA + numberB);
