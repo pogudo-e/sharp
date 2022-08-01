35c823fded5cf4fc8554ebac7a25a908a2e59d44
Evgeny Pogudo <sensesbit@gmail.com>
2022-08-01 17:28:31 +0300

    example010 and oll

--- example010/Program.cs
+++ example010/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {1,2,83,14,55,6,27,8,9};
+
+int n = array.Length;
+int find = 14;
+int index = 0;
+
+while(index < n )
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
